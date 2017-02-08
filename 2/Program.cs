608c349daeb4185b5dd46bfde0f0988560ab8707
schwaigerbm <schwaigerbm@gmail.com>
2017-02-08 15:21:44 +0100

    2 => Startup Hello World Example

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.AspNetCore.Http;
 
 namespace ConsoleApplication
 {
@@ -6,7 +12,27 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
+
+            var host = new WebHostBuilder()
+            
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseStartup<Startup>()
+            .Build();
+
+            host.Run();
+
+
             Console.WriteLine("Hello World!");
+        }
+
+        public class Startup{
+    
+            public void Configure(IApplicationBuilder app, IHostingEnvironment env){
+                app.Run(async(context) => await context.Response.WriteAsync("Hello World ASP.NET"));
+
+            }
+
         }
     }
 }
